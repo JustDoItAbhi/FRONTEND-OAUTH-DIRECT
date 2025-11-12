2f106ce01ab0a4b7964029c7be8ab42c5109c50e
Admin <erarvinder2012@gmail.com>
2025-11-12 19:00:35 +0200

    defineConfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 10000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 10000,
+    allowedHosts: [
+      'frontend-oauth2-flow-and-direct-oauth2.onrender.com'
+    ]
+  }
 })
